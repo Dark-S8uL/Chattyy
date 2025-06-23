e02a6ab19dd76a03b73e70a532b962672b352d84
Dark-S8uL <pr262004@gmail.com>
2025-06-23 17:16:17 +0530

    Update 4

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,37 +1,37 @@
-import React, { useEffect } from 'react'
-import Navbar from "./components/Navbar" ;
+import Navbar from "./components/Navbar";
 
-import HomePage from "./pages/HomePage" ;
-import SignUpPage from "./pages/SignUpPage" ;
-import LoginPage from "./pages/LoginPage" ;
-import SettingsPage from "./pages/SettingsPage" ;
-import ProfilePage from "./pages/ProfilePage" ;
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
 
-import { Routes, Route, Navigate } from "react-router-dom" ;
-import { useAuthStore } from './store/useAuthStore';
-import { useThemeStore } from './store/useThemeStore';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
+import { useEffect } from "react";
 
-import { Loader } from "lucide-react" ;
-import { Toaster } from 'react-hot-toast';
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore() ;
-  const { theme } = useThemeStore() ;
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+  const { theme } = useThemeStore();
 
   console.log({ onlineUsers });
 
   useEffect(() => {
-    checkAuth() ;
-  }, [checkAuth]) ;
+    checkAuth();
+  }, [checkAuth]);
 
-  console.log({ authUser }) ;
+  console.log({ authUser });
 
-  if(isCheckingAuth && !authUser) 
+  if (isCheckingAuth && !authUser)
     return (
-      <div className='flex items-center justify-center h-screen'>
+      <div className="flex items-center justify-center h-screen">
         <Loader className="size-10 animate-spin" />
       </div>
-    ) ;
+    );
 
   return (
     <div data-theme={theme}>
@@ -47,7 +47,6 @@ const App = () => {
 
       <Toaster />
     </div>
-  ) ;
-} ;
-
-export default App ;
+  );
+};
+export default App;
